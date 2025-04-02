0e8f70d074d20484a7eff00a855b7eb9d7e34762
Karen Lean Kay Cabarrubias <karenleankay.cabarrubias@cit.edu>
2025-04-02 22:29:55 +0800

    API endpoints for post and get user entity

--- backend/src/main/java/edu/cit/campuscart/service/UserService.java
+++ backend/src/main/java/edu/cit/campuscart/service/UserService.java
@@ -1,13 +1,41 @@
 package edu.cit.campuscart.service;
 
-import edu.cit.campuscart.entity.UserEntity;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 
+import javax.naming.NameAlreadyBoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.cit.campuscart.entity.UserEntity;
+import edu.cit.campuscart.repository.UserRepository;
+
+@Service
 public class UserService {
     @Autowired
-    private UserEntity userEntity;
+    private UserRepository userRepo;
 
     public UserService() {
         super();
     }
+    
+  //CREATE
+  	public UserEntity postUserRecord(UserEntity user) throws NameAlreadyBoundException {
+  		if(userRepo.existsById(user.getUsername())) {
+  			throw new NameAlreadyBoundException("Username " + user.getUsername() + " is already taken. Input another username.");
+  		}
+  		
+  		if (isEmailExists(user.getEmail())) {
+              throw new NameAlreadyBoundException("Email already exists");
+          }
+  		
+  		return userRepo.save(user);
+  	}
+  	
+  	public boolean isEmailExists(String email) {
+        return userRepo.existsByEmail(email);
+    }
+
+	public List<UserEntity> getAllUsers() {
+		return userRepo.findAll();
+	}
 }
