f1c66250275e4b575f03a20c4dc8b70972769a24
Chizzwiz <chrizzzaarnie@gmail.com>
2025-04-16 01:33:28 +0800

    New Feature Added (not done yet): View Card for Buyer and Seller

---
+++ frontend_mobile/app/src/main/java/edu/cit/campuscart/ProductDetailDialogFragment.kt
@@ -0,0 +1,32 @@
+package edu.cit.campuscart
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import edu.cit.campuscart.models.Products
+
+class ProductDetailDialogFragment {
+//Not Yet Done, Still Working On It
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.view_by_buyer, container, false)
+
+        // Bind data to views
+        view.findViewById<TextView>(R.id.product_name).text = product.name
+        view.findViewById<TextView>(R.id.product_description).text = product.pdtDescription
+        view.findViewById<TextView>(R.id.product_price).text = "₱${product.buyPrice}"
+
+        val imageView = view.findViewById<ImageView>(R.id.product_image)
+        // Load image with Glide or Picasso if you have URLs
+        // Glide.with(this).load(product.imageUrl).into(imageView)
+
+        return view
+    }
+}
+
